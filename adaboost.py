4f5503972218c250c9775d82c9114148d806ba0b
ateexd <ateendraramesh@gmail.com>
2019-04-24 22:53:03 -0400

    modified image_utils

--- adaboost.py
+++ adaboost.py
@@ -1,9 +1,12 @@
-def adaboost(data: List):
+import numpy as np 
+
+def adaboost(data: List, T: int):
     """This is an implementation of the AdaBoost algorithm mentioned in the 
     Viola-Jones paper.
     
     Arguments:
-        data {List} -- Right now, just a list of features.
+        data {List} -- Right now, a tuple of x & y
+        T {int} -- Number of features needed 
 
     Returns:
         weights -- Weights for the strongest classifer
@@ -12,4 +15,17 @@ def adaboost(data: List):
     # For now, assuming that the x & y are packed into data
     x, y = data
 
-    # TODO - Finish this
+    # Get number of samples
+    n = len(y)
+    m = len(y[y == 0])
+    l = n - m
+
+    w = np.ones_like(y)
+    w[y == 1] /= l
+    w[y == 0] /= m
+
+    for t in range(T):
+        w /= np.sum(w)
+        sorted_indices = np.argsort(x[i])
+        features_sorted = x[i][sorted_indices]
+
--- image_utils.py
+++ image_utils.py
@@ -1,6 +1,28 @@
 import cv2
 import numpy as np
 
+from collections import Counter
+
+class box:
+    def __init__(self, x: int, y: int, l: int, b: int):
+        """A box in the integral image, given start points and dimensions.
+
+        Arguments:
+            x {int} -- x co-ordinate of origin 
+            y {int} -- y co-ordinate of origin 
+            l {int} -- Length of box
+            b {int} -- Width of box
+        """
+        self.x = x
+        self.y = y
+        self.l = l
+        self.b = b
+
+    def __str__(self):
+        return "Box: x - " + str(self.x) + " y - " + str(self.y) + \
+             " l - " + str(self.l) + " b - " + str(self.b)
+
+
 
 def integral_image(img: np.ndarray) -> np.ndarray:
     """This function returns the integral image for a given image.
@@ -14,6 +36,12 @@ def integral_image(img: np.ndarray) -> np.ndarray:
     Returns:
         integral_image {np.ndarray} -- The integral image
     """
+    if len(img.shape) == 2:
+        m, n = img.shape
+        integral_image = np.zeros((m + 1, n + 1))
+        integral_image[1:, 1:] = img.cumsum(0).cumsum(1)
+        return integral_image
+
     # Get image shape
     m, n, o = img.shape
 
@@ -24,6 +52,20 @@ def integral_image(img: np.ndarray) -> np.ndarray:
     integral_image[1:, 1:, :] = img.cumsum(0).cumsum(1)
 
     return integral_image
+
+
+def get_integral_sum_box(integral_img: np.ndarray, box: box) -> float:
+    """Same as get_integral_sum but with box
+
+    Arguments:
+        integral_image {np.ndarray} -- The integral image
+        box {box} -- box object
+
+    Returns:
+        feature -- Same as get_integral_sum
+    """
+    return get_integral_sum(integral_img, box.x, box.y, box.b, box.l)
+
 
 def get_integral_sum(integral_img: np.ndarray, x: int, y: int, h: int, w: int) -> float:
     """Gets summation of a table in the integral image
@@ -36,13 +78,18 @@ def get_integral_sum(integral_img: np.ndarray, x: int, y: int, h: int, w: int) -
         h {int} -- Height of the rectangle
 
     Returns:
-        float -- [description]
+        feature -- Difference in co-ords
     """
     feature = integral_img[y + h][x + w] + integral_img[y][x] - \
         integral_img[y + h][x] - integral_img[y][x + w]
-    return feature
+    
+    try:
+        _ = iter(feature)
+        return np.sqrt(np.sum(feature ** 2))
+    except:
+        return feature
 
-def get_features(img: np.ndarray) -> List:
+def get_features(img: np.ndarray) -> list:
     """This computes the features, given the rectangular boxes
 
     Arguments:
@@ -52,13 +99,59 @@ def get_features(img: np.ndarray) -> List:
         features {List} -- List of features computed
     """
     integral_img = integral_image(img)
-    
-    m, n, o = integral_img.shape
 
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            for k in range(0, m, i):
-                for l in range(0, n, j):
-                    pass
-    # TODO - implement this
+    shape = integral_img.shape
+    m, n = shape[0], shape[1]
+
+    features = []
+
+    feature_shape = [[2, 1],
+                     [1, 2],
+                     [3, 1], 
+                     [1, 3], 
+                     [2, 2]]
+
+    c1, c2, c3, c4, c5 = 0, 0, 0, 0, 0
+    count = 0
+    for f in feature_shape:
+        x, y = f[0], f[1]
+        for i in range(m - x + 1):
+            for j in range(n - y + 1):
+                for k in range(x, m - i, x):
+                    for l in range(y, n - j, y):
+                        count += 1
+                        if f == [2, 1]:
+                            box1 = get_integral_sum_box(integral_img, box(i, j, k // 2, l))
+                            box2 = get_integral_sum_box(integral_img, box(i + k // 2, j, k // 2, l))
+                            features.append(box1 - box2)
+
+                        if f == [1, 2]:
+                            box1 = get_integral_sum_box(integral_img, box(i, j, k, l // 2))
+                            box2 = get_integral_sum_box(integral_img, box(i, j + l // 2, k, l // 2))
+                            features.append(box2 - box1)
+                        
+                        if f == [3, 1]:
+                            box1 = get_integral_sum_box(integral_img, box(i, j, k // 3, l))
+                            box2 = get_integral_sum_box(integral_img, box(i + k // 3, j, k // 3, l))
+                            box3 = get_integral_sum_box(integral_img, box(i + 2 * k // 3, j, k // 3, l))
+                            features.append(box2 - box1 - box3)
+                        
+                        if f == [1, 3]:
+                            box1 = get_integral_sum_box(integral_img, box(i, j, k, l // 3))
+                            box2 = get_integral_sum_box(integral_img, box(i, j + l // 3 , k, l // 3))
+                            box3 = get_integral_sum_box(integral_img, box(i, j + 2 * l // 3, k, l // 3))
+                            features.append(box2 - box1 - box3)
+                        
+                        if f == [2, 2]:
+                            box1 = get_integral_sum_box(integral_img, box(i, j, k // 2, l // 2))
+                            box2 = get_integral_sum_box(integral_img, box(i + k // 2, j, k // 2, l // 2))
+                            box3 = get_integral_sum_box(integral_img, box(i, j + l // 2, k // 2, l // 2))
+                            box4 = get_integral_sum_box(integral_img, box(i + k // 2, j + l // 2, k // 2, l // 2))
+                            features.append(box1 - box2 - box3 + box4)
+
+    return features
+
+if __name__ == "__main__":
+    img = cv2.imread("/Users/ateendraramesh/Downloads/face.png")
+    features = np.array(get_features(np.ones((24, 24))))
     
