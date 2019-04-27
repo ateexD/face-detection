f6c33799abca9ea6787fd9b99092532853df29e3
ateexd <ateendraramesh@gmail.com>
2019-04-27 00:00:16 -0400

    added adaboost

--- adaboost.py
+++ adaboost.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-def get_epsilon_and_parity(feature_col: np.ndarray, y: np.ndarray, w: np.ndarray) -> tuple:
+def get_theta_and_parity(feature_col: np.ndarray, y: np.ndarray, w: np.ndarray) -> tuple:
     """
     
     Arguments:
@@ -33,24 +33,23 @@ def get_epsilon_and_parity(feature_col: np.ndarray, y: np.ndarray, w: np.ndarray
         s_p_list.append(s_p)
     
     error = 1e10
-    epsilon, parity = 0, 0
+    theta, parity = 0, 0
 
     for i in range(len(y)):
         e1 = s_p_list[i] + t_m - s_m_list[i]
         e2 = s_m_list[i] + t_p - s_p_list[i]
         if e1 < error:
             error = e1
-            epsilon = feature_col[i]
+            theta = feature_col[i]
             parity = -1
         elif e2 < error:
             error = e2
-            epsilon = feature_col[i]
+            theta = feature_col[i]
             parity = 1
-    return (epsilon, parity)
+    return (theta, parity)
 
 
-
-def adaboost(data: list, T: int):
+def adaboost(data: list, T: int) -> dict:
     """
     This is an implementation of the AdaBoost algorithm mentioned in the 
     Viola-Jones paper.
@@ -60,7 +59,7 @@ def adaboost(data: list, T: int):
         T {int} -- Number of features needed 
 
     Returns:
-        weights -- Weights for the strongest classifer
+        classifier_list -- List of weak classifiers
     """
 
     # For now, assuming that the x & y are packed into data
@@ -72,10 +71,57 @@ def adaboost(data: list, T: int):
     l = n - m
     
     w = np.ones_like(y)
-    w[y == 1] /= (2 * l)
-    w[y == 0] /= (2 * m)
+    w[y == 1] /= (2. * l)
+    w[y == 0] /= (2. * m)
+    error_so_far = 1e10
 
+    cache = {}
+
+    classifier_list = []
+    
     for t in range(T):
+
         w /= np.sum(w)
+
+        for i in range(x.shape[1]):
+            feature_col = x[:, i]
+            theta, parity = get_theta_and_parity(feature_col, y, w)
+            error = 0.
+            
+            for j in range(x.shape[0]):
+                if parity * theta > parity * feature_col[j]:
+                    pred = 1
+                else:
+                    pred = 0
+                error += np.abs(pred - y) * w[j]
+            
+                if error < error_so_far:
+                    cache["theta"] = theta
+                    cache["parity"] = parity
+                    cache["error"] = error
+                    cache["index"] = j
+                    error_so_far = error
+
+        beta = cache["error"] / (1 - cache["error"])
+        alpha = np.log(1 / beta)
+
+        parity, theta = cache["parity"], cache["theta"]
+        for i in range(x.shape[0]):
+            if parity * theta > parity * x[i, cache["index"]]:
+                pred = 1
+            else:
+                pred = 0
+            error = np.abs(pred - y)
+            w[i] *= beta ** (1 - error)
+        
+        classifier_dict = {
+            "theta": theta,
+            "alpha": alpha,
+            "parity": parity,
+            "index": cache["index"]
+        }
+        classifier_list.append(classifier_dict)
+        return classifier_list
+
 
 
